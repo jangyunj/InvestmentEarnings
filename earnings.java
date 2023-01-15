bc4a838d852dff9cfc0eebee12687bd160868892
Yun Jang <jang.yunj7@gmail.com>
2023-01-14 23:49:00 -0500

    second commit

---
+++ earnings.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class earnings {
+    public static void main(String[] args) {
+        System.out.println("How much can you spend?");
+        Scanner scan = new Scanner(System.in);
+        double total = scan.nextDouble();
+        double sum = 0;
+        int i = 0;
+
+    }
+}
